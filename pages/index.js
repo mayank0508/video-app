83dead715dc54deeae42302d56811f0aba8f926b
mayank0508 <thakurmayank0508@gmail.com>
2021-10-11 09:39:40 +0530

    UI styled

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { sanityClient } from '../sanity';
+import { sanityClient } from '../sanity'
 
 const Home = ({ videos }) => {
   console.log(videos);
---
+++ pages/video/[slug].js
@@ -0,0 +1,74 @@
+import { sanityClient } from '../../sanity';
+
+const Video = ({
+  title,
+  asset,
+  thumbnail,
+  date,
+  description,
+  author,
+  interaction
+}) => {
+  console.log(title);
+  return (
+    <div className="video">
+      <h3>{title}</h3>
+      <div className="video-info">
+          <h6>Premiumed on {date}</h6>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async pageContext => {
+  const pageSlug = pageContext.query.slug;
+  const query = `*[ _type == "video" && slug.current == $pageSlug ][0]{
+    title,
+    id,
+    'asset': videoAsset.asset->,
+    thumbnail,
+    date,
+    description,
+    author->{
+        id,
+        username,
+        slug,
+        avatar,
+        subscribers,
+    },
+    interaction[]{
+        ...,
+        commenter->{
+            id,
+            username,
+            slug,
+            avatar,
+            subscribers,
+            }
+         }
+     }`;
+
+  const video = await sanityClient.fetch(query, { pageSlug });
+
+  if (!video) {
+    return {
+      props: null,
+      notFound: true
+    };
+  } else {
+    return {
+      props: {
+        title: video.title,
+        id: video.id,
+        video: video.asset,
+        thumbnail: video.thumbnail,
+        date: video.date,
+        description: video.description,
+        author: video.author,
+        interaction: video.interaction
+      }
+    };
+  }
+};
+
+export default Video;
